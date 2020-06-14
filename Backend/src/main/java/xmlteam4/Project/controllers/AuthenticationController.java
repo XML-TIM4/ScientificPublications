e2228547f12bd7e21004f205c53c6a83ff9df696
Dragan Ćulibrk <33953768+draganculibrk9@users.noreply.github.com>
2020-06-14 09:32:34 +0200

    Update token utils

--- Backend/src/main/java/xmlteam4/Project/controllers/AuthenticationController.java
+++ Backend/src/main/java/xmlteam4/Project/controllers/AuthenticationController.java
@@ -6,6 +6,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import xmlteam4.Project.DTOs.LoginDTO;
 import xmlteam4.Project.DTOs.UserDTO;
 import xmlteam4.Project.model.AuthenticationResponse;
+import xmlteam4.Project.model.TUser;
 import xmlteam4.Project.security.TokenUtils;
 import xmlteam4.Project.services.AuthenticationService;
 import xmlteam4.Project.services.UserDetailsServiceImpl;
@@ -36,13 +39,18 @@ public class AuthenticationController {
     @PostMapping("/login")
     @PreAuthorize("!(hasAuthority('ROLE_AUTHOR') or hasAuthority('ROLE_EDITOR'))")
     public ResponseEntity login(@Valid @RequestBody LoginDTO loginDTO) {
+        final Authentication authentication;
         try {
-            UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(
-                    loginDTO.getEmail(), loginDTO.getPassword());
-            authenticationManager.authenticate(token);
-            UserDetails details = userDetailsService.loadUserByUsername(loginDTO.getEmail());
-            String temp = tokenUtils.generateToken(details);
-            return new ResponseEntity<>(new AuthenticationResponse(temp), HttpStatus.OK);
+            authentication =
+                    authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginDTO.getEmail(),
+                            loginDTO.getPassword()));
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+
+            TUser user = (TUser) authentication.getPrincipal();
+
+            String jwt = tokenUtils.generateToken(user.getUsername());
+
+            return new ResponseEntity<>(jwt, HttpStatus.OK);
         } catch (Exception e) {
             return new ResponseEntity<>("Invalid email or password", HttpStatus.BAD_REQUEST);
         }
@@ -51,7 +59,6 @@ public class AuthenticationController {
     @PostMapping("/register")
     @PreAuthorize("!(hasAuthority('ROLE_AUTHOR') or hasAuthority('ROLE_EDITOR'))")
     public ResponseEntity<Object> register(@Valid @RequestBody UserDTO user) {
-        System.out.println(user.toString());
         try {
             return new ResponseEntity<>(authenticationService.register(user), HttpStatus.OK);
         } catch (Exception e) {
--- Backend/src/main/java/xmlteam4/Project/security/TokenUtils.java
+++ Backend/src/main/java/xmlteam4/Project/security/TokenUtils.java
@@ -4,49 +4,99 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
+import xmlteam4.Project.model.TUser;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 @Component
 public class TokenUtils {
+    @Value("Scientific-Publications")
+    private String APP_NAME;
+
     @Value("myXAuthSecret")
     private String secret;
 
     @Value("18000") //in seconds (5 hours)
     private Long expiration;
 
-    public String getEmailFromToken(String token) {
-        String email;
-        try {
-            Claims claims = this.getClaimsFromToken(token);
-            email = claims.getSubject();
-        } catch (Exception e) {
-            email = null;
-        }
-        return email;
+    @Value("Authorization")
+    private String AUTH_HEADER;
+
+
+    public String generateToken(String username) {
+        return Jwts.builder().setIssuer(APP_NAME).setSubject(username).setIssuedAt(new Date())
+                .setExpiration(generateExpirationDate()).signWith(SignatureAlgorithm.HS512, secret).compact();
     }
 
-    private Claims getClaimsFromToken(String token) {
+    private Date generateExpirationDate() {
+        return new Date(new Date().getTime() + expiration * 1000);
+    }
+
+    public Boolean validateToken(String token, UserDetails userDetails) {
+        TUser user = (TUser) userDetails;
+        final String username = getUsernameFromToken(token);
+        final Date created = getIssuedAtDateFromToken(token);
+
+        return username != null && username.equals(userDetails.getUsername());
+    }
+
+    private Boolean isTokenExpired(String token) {
+        final Date expiration = this.getExpirationDateFromToken(token);
+        return expiration.before(new Date());
+    }
+
+    // Functions for getting data from token
+
+    private Claims getAllClaimsFromToken(String token) {
         Claims claims;
         try {
-            claims = Jwts.parser().setSigningKey(this.secret)
-                    .parseClaimsJws(token).getBody();
+            claims = Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
         } catch (Exception e) {
             claims = null;
         }
         return claims;
     }
 
+    public String getUsernameFromToken(String token) {
+        String username;
+        try {
+            final Claims claims = this.getAllClaimsFromToken(token);
+            username = claims.getSubject();
+        } catch (Exception e) {
+            username = null;
+        }
+        return username;
+    }
+
+    public Date getIssuedAtDateFromToken(String token) {
+        Date issueAt;
+        try {
+            final Claims claims = this.getAllClaimsFromToken(token);
+            issueAt = claims.getIssuedAt();
+        } catch (Exception e) {
+            issueAt = null;
+        }
+        return issueAt;
+    }
+
+    public String getAudienceFromToken(String token) {
+        String audience;
+        try {
+            final Claims claims = this.getAllClaimsFromToken(token);
+            audience = claims.getAudience();
+        } catch (Exception e) {
+            audience = null;
+        }
+        return audience;
+    }
+
     public Date getExpirationDateFromToken(String token) {
         Date expiration;
         try {
-            final Claims claims = this.getClaimsFromToken(token);
+            final Claims claims = this.getAllClaimsFromToken(token);
             expiration = claims.getExpiration();
         } catch (Exception e) {
             expiration = null;
@@ -54,30 +104,19 @@ public class TokenUtils {
         return expiration;
     }
 
-    private boolean isTokenExpired(String token) {
-        final Date expiration = this.getExpirationDateFromToken(token);
-        return expiration.before(new Date(System.currentTimeMillis()));
+    // Functions for getting JWT token out of HTTP request
+
+    public String getToken(HttpServletRequest request) {
+        String authHeader = getAuthHeaderFromHeader(request);
+
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            return authHeader.substring(7);
+        }
+
+        return null;
     }
 
-    public boolean validateToken(String token, UserDetails userDetails) {
-        final String email = getEmailFromToken(token);
-        return email.equals(userDetails.getUsername())
-                && !isTokenExpired(token);
-    }
-
-    public String generateToken(UserDetails userDetails) {
-
-        String authorities = userDetails.getAuthorities().stream()
-                .map(GrantedAuthority::getAuthority)
-                .collect(Collectors.joining(","));
-
-        Map<String, Object> claims = new HashMap<>();
-        claims.put("sub", userDetails.getUsername());
-        claims.put("audience", "web");
-        claims.put("created", new Date(System.currentTimeMillis()));
-        claims.put("authorities", authorities);
-        return Jwts.builder().setClaims(claims)
-                .setExpiration(new Date(System.currentTimeMillis() + expiration * 1000))
-                .signWith(SignatureAlgorithm.HS512, secret).compact();
+    public String getAuthHeaderFromHeader(HttpServletRequest request) {
+        return request.getHeader(AUTH_HEADER);
     }
 }
