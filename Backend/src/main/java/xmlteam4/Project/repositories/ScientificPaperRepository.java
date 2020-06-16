b0caeb348ce9880dfbfc46a9791a04bf4539f73c
Dragan Ćulibrk <33953768+draganculibrk9@users.noreply.github.com>
2020-06-16 12:43:28 +0200

    Fix advanced scientific paper search

--- Backend/src/main/java/xmlteam4/Project/repositories/ScientificPaperRepository.java
+++ Backend/src/main/java/xmlteam4/Project/repositories/ScientificPaperRepository.java
@@ -99,8 +99,11 @@ public class ScientificPaperRepository {
             if (papersSet != null) {
                 List<XMLResource> resources = queryService.extractAllResources(papersSet);
 
+                String id;
                 for (XMLResource res : resources) {
-                    searchResultDTO.getOtherPaperIds().add(res.getContent().toString());
+                    id = res.getContent().toString();
+                    id = id.replace("3030/ScientificPaperDataset", "8080");
+                    searchResultDTO.getOtherPaperIds().add(id);
                     ((EXistResource) res).freeResources();
                 }
             }
@@ -128,8 +131,11 @@ public class ScientificPaperRepository {
             if (ownPapers != null) {
                 List<XMLResource> resources = queryService.extractAllResources(ownPapersSet);
 
+                String id;
                 for (XMLResource res : resources) {
-                    searchResultDTO.getOwnPaperIds().add(res.getContent().toString());
+                    id = res.getContent().toString();
+                    id = id.replace("3030/ScientificPaperDataset", "8080");
+                    searchResultDTO.getOwnPaperIds().add(id);
                     ((EXistResource) res).freeResources();
                 }
 
@@ -138,8 +144,11 @@ public class ScientificPaperRepository {
             if (otherPapersSet != null) {
                 List<XMLResource> resources = queryService.extractAllResources(otherPapersSet);
 
+                String id;
                 for (XMLResource res : resources) {
-                    searchResultDTO.getOtherPaperIds().add(res.getContent().toString());
+                    id = res.getContent().toString();
+                    id = id.replace("3030/ScientificPaperDataset", "8080");
+                    searchResultDTO.getOtherPaperIds().add(id);
                     ((EXistResource) res).freeResources();
                 }
             }
@@ -151,22 +160,33 @@ public class ScientificPaperRepository {
     }
 
     public SearchResultDTO advancedSearch(SearchDTO searchDTO) {
-        String condition = "graph ?g { ?s <https://schema.org/datePublished> {accepted} . ?s" +
-                " <https://schema.org/dateCreated> {received} . ?s <https://schema.org/dateModified> {revised} . ?s " +
-                "<https://schema.org/version> {version} . ?s <https://schema.org/creativeWorkStatus> accepted . ?s " +
-                "<https://schema.org/genre> {category} . ?s <https://schema.org/keywords> ?keywords . FILTER" +
-                "(CONTAINS(UCASE(str(?keywords)), UCASE({keywords})))";
+        String condition = "graph ?g { ?s <https://schema.org/datePublished> ${accepted} . " +
+                "?s <https://schema.org/dateCreated> ${received} . " +
+                "?s <https://schema.org/dateModified> ${revised} . " +
+                "?s <https://schema.org/version> ${version} . " +
+                "?s <https://schema.org/creativeWorkStatus> ?status . " +
+                "?s <https://schema.org/genre> ${category} . " +
+                "?s <https://schema.org/keywords> ?keywords . " +
+                "FILTER(str(?status) = 'accepted') . " +
+                "FILTER(CONTAINS(UCASE(str(?keywords)), UCASE(str(${keywords}))))}";
 
         HashMap<String, String> substitutionMap = new HashMap<>();
-        substitutionMap.put("accepted", searchDTO.getAccepted() != null ? searchDTO.getAccepted().toString() : "?o");
-        substitutionMap.put("received", searchDTO.getReceived() != null ? searchDTO.getReceived().toString() : "?o");
-        substitutionMap.put("revised", searchDTO.getRevised() != null ? searchDTO.getRevised().toString() : "?o");
-        substitutionMap.put("version", searchDTO.getVersion() != null ? searchDTO.getVersion() : "?o");
-        substitutionMap.put("category", searchDTO.getCategory() != null ? searchDTO.getCategory().toString() : "?o");
-        substitutionMap.put("keywords", String.join(",", searchDTO.getKeywords()));
+        substitutionMap.put("accepted", searchDTO.getAccepted() != null ? searchDTO.getAccepted().toString() :
+                "?accepted");
+        substitutionMap.put("received", searchDTO.getReceived() != null ? searchDTO.getReceived().toString() :
+                "?received");
+        substitutionMap.put("revised", searchDTO.getRevised() != null ? searchDTO.getRevised().toString() : "?revised");
+        substitutionMap.put("version", searchDTO.getVersion() != null ? searchDTO.getVersion() : "?version");
+        substitutionMap.put("category", searchDTO.getCategory() != null ? searchDTO.getCategory().toString() :
+                "?category");
+
+        if (searchDTO.getKeywords().isEmpty())
+            substitutionMap.put("keywords", "''");
+        else
+            substitutionMap.put("keywords", String.join(",", searchDTO.getKeywords()));
 
         StringSubstitutor substitutor = new StringSubstitutor(substitutionMap);
-
+        System.out.println(substitutor.replace(condition));
         ArrayList<String> graphNames = sparqlService.queryAll(substitutor.replace(condition));
         SearchResultDTO searchResult = new SearchResultDTO();
         searchResult.setOtherPaperIds(graphNames);
@@ -175,21 +195,33 @@ public class ScientificPaperRepository {
     }
 
     public SearchResultDTO advancedSearch(SearchDTO searchDTO, String authorID) {
-        String condition = "graph ?g { ?s <https://schema.org/datePublished> {accepted} . ?s" +
-                " <https://schema.org/dateCreated> {received} . ?s <https://schema.org/dateModified> {revised} . ?s " +
-                "<https://schema.org/version> {version} . ?s <https://schema.org/creativeWorkStatus> {status} . ?s " +
-                "<https://schema.org/genre> {category} . ?s <https://schema.org/keywords> ?keywords . ?s " +
-                "<https://schema.org/author> {author} . FILTER" +
-                "(CONTAINS(UCASE(str(?keywords)), UCASE({keywords})))";
+        String condition = "graph ?g { ?s <https://schema.org/datePublished> ${accepted} . " +
+                "?s <https://schema.org/dateCreated> ${received} . " +
+                "?s <https://schema.org/dateModified> ${revised} . " +
+                "?s <https://schema.org/version> ${version} . " +
+                "?s <https://schema.org/creativeWorkStatus> ?status . " +
+                "?s <https://schema.org/genre> ${category} . " +
+                "?s <https://schema.org/keywords> ?keywords . " +
+                "?s <https://schema.org/author> ?author . " +
+                "FILTER(str(?author) = '${author}') . " +
+                "FILTER(CONTAINS(UCASE(str(?keywords)), UCASE(str(${keywords}))))}";
 
         HashMap<String, String> substitutionMap = new HashMap<>();
-        substitutionMap.put("accepted", searchDTO.getAccepted() != null ? searchDTO.getAccepted().toString() : "?o");
-        substitutionMap.put("received", searchDTO.getReceived() != null ? searchDTO.getReceived().toString() : "?o");
-        substitutionMap.put("revised", searchDTO.getRevised() != null ? searchDTO.getRevised().toString() : "?o");
-        substitutionMap.put("version", searchDTO.getVersion() != null ? searchDTO.getVersion() : "?o");
-        substitutionMap.put("category", searchDTO.getCategory() != null ? searchDTO.getCategory().toString() : "?o");
-        substitutionMap.put("keywords", String.join(",", searchDTO.getKeywords()));
-        substitutionMap.put("status", searchDTO.getStatus() != null ? searchDTO.getStatus().name() : "?o");
+        substitutionMap.put("accepted", searchDTO.getAccepted() != null ? searchDTO.getAccepted().toString() :
+                "?accepted");
+        substitutionMap
+                .put("received", searchDTO.getReceived() != null ? searchDTO.getReceived().toString() : "?received");
+        substitutionMap.put("revised", searchDTO.getRevised() != null ? searchDTO.getRevised().toString() : "?revised");
+        substitutionMap.put("version", searchDTO.getVersion() != null ? searchDTO.getVersion() : "?version");
+        substitutionMap.put("category", searchDTO.getCategory() != null ? searchDTO.getCategory().toString() :
+                "?category");
+
+        if (searchDTO.getKeywords().isEmpty())
+            substitutionMap.put("keywords", "''");
+        else
+            substitutionMap.put("keywords", String.join(",", searchDTO.getKeywords()));
+
+        substitutionMap.put("status", searchDTO.getStatus() != null ? searchDTO.getStatus().name() : "?status");
         substitutionMap.put("author", authorID);
 
 
@@ -198,12 +230,17 @@ public class ScientificPaperRepository {
         SearchResultDTO searchResult = new SearchResultDTO();
         searchResult.setOwnPaperIds(graphNames);
 
-        condition = "graph ?g { ?s <https://schema.org/datePublished> {accepted} . ?s" +
-                " <https://schema.org/dateCreated> {received} . ?s <https://schema.org/dateModified> {revised} . ?s " +
-                "<https://schema.org/version> {version} . ?s <https://schema.org/creativeWorkStatus> accepted . ?s " +
-                "<https://schema.org/genre> {category} . ?s <https://schema.org/keywords> ?keywords . ?s <https://schema" +
-                ".org/author> ?a . FILTER" +
-                "(CONTAINS(UCASE(str(?keywords)), UCASE({keywords}))) . FILTER(?a != ?{author})";
+        condition = "graph ?g { ?s <https://schema.org/datePublished> ${accepted} . " +
+                "?s <https://schema.org/dateCreated> ${received} . " +
+                "?s <https://schema.org/dateModified> ${revised} . " +
+                "?s <https://schema.org/version> ${version} . " +
+                "?s <https://schema.org/creativeWorkStatus> ?status . " +
+                "?s <https://schema.org/genre> ${category} . " +
+                "?s <https://schema.org/keywords> ?keywords . " +
+                "?s <https://schema.org/author> ?author . " +
+                "FILTER(str(?status) = 'accepted') . " +
+                "FILTER(CONTAINS(UCASE(str(?keywords)), UCASE(str(${keywords})))) . " +
+                "FILTER(?author != '${author}')}";
 
         substitutionMap.remove("status");
         substitutor = new StringSubstitutor(substitutionMap);
